fdfc92e68995380af5a98b150f08268ec3067454
Federico Urquiza <federicourquizaranieri@gmail.com>
2024-04-15 19:43:35 -0300

    Cambios esteticos

--- src/Square.js
+++ src/Square.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Square({ value, onClick }) {
+    let color;
+  
+  // Define el color de fondo basado en el valor
+  if (value === '#') {
+    color = 'black'; // Cambia el color a tu preferencia
+  } else {
+    color = 'white'; // Otro color de fondo para otros valores
+  }
     return (
-        <button className="square" onClick={onClick}>
+        <button className="square" onClick={onClick} style={{background:color, borderRadius: '5px', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', transition: 'background-color 0.3s'}}>
             {value !== '_' ? value : null}
         </button>
     );
