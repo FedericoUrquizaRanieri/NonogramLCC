137241ae23af6be9670a56eefad5f92426bb82b3
AleGonzaa <gonzalezalejo918@gmail.com>
2024-04-16 10:27:57 -0300

    colores

--- src/Square.js
+++ src/Square.js
@@ -4,13 +4,13 @@ function Square({ value, onClick }) {
     let color;
   
   // Define el color de fondo basado en el valor
-  if (value === '#') {
-    color = 'black'; // Cambia el color a tu preferencia
+  if (value === '') {
+    color = '#344862'; // Cambia el color a tu preferencia
   } else {
     color = 'white'; // Otro color de fondo para otros valores
   }
     return (
-        <button className="square" onClick={onClick} style={{background:color, borderRadius: '5px', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', transition: 'background-color 0.3s'}}>
+        <button className="square" onClick={onClick} style={{background:color, boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', transition: 'background-color 0.3s'}}>
             {value !== '_' ? value : null}
         </button>
     );
