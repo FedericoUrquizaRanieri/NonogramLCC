f5b8df57563e6926194e81331b4ad00df579eb31
Federico Urquiza <federicourquizaranieri@gmail.com>
2024-04-20 19:23:51 -0300

    Prolog documentation

--- pengines_server/apps/proylcc/proylcc.pl
+++ pengines_server/apps/proylcc/proylcc.pl
@@ -18,19 +18,23 @@ replace(X, XIndex, Y, [Xi|Xs], [Xi|XsY]):-
     XIndexS is XIndex - 1,
     replace(X, XIndexS, Y, Xs, XsY).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%documentar
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-% put(+Content, +Pos, +RowsClues, +ColsClues, +Grid, -NewGrid, -RowSat, -ColSat).
+% rowToList(+RowN, +Row, -Out).
 %
+% Given a grid and a position returns the list in the position RowN of the grid
 rowToList(0,[H|_Row],H).
 rowToList(RowN,[_H|Row],Out):-
 	RowNX is RowN-1,
     rowToList(RowNX,Row,Out).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%documentar
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-% put(+Content, +Pos, +RowsClues, +ColsClues, +Grid, -NewGrid, -RowSat, -ColSat).
+% colToList(+ColN, +Grid, -ColList).
 %
+% colToListSearch(+ColN,+Row,-Out).
+%
+% This methods create a list of the elements of a column in a grid
 colToList(_ColN,[],[]).
 colToList(ColN,[Row|Grid],[Out|ColList]):-
 	colToListSearch(ColN,Row,Out),
@@ -42,18 +46,21 @@ colToListSearch(ColN,[_H|Row],Out):-
 	ColNX is ColN-1,
 	colToListSearch(ColNX,Row,Out).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%documentar
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-% put(+Content, +Pos, +RowsClues, +ColsClues, +Grid, -NewGrid, -RowSat, -ColSat).
+% checkList(+RowN, +RowsClues, +NewRow,-Out).
+%
+%Given a list, the clues and  a position finds the clue of that list and checks if it is correct
 %
 checkList(RowN, RowsClues, NewRow,Out):-
 	findClue(RowsClues,RowN,Ret),
 	searchClue(Ret,NewRow,Out).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%documentar
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-% put(+Content, +Pos, +RowsClues, +ColsClues, +Grid, -NewGrid, -RowSat, -ColSat).
+% findClue(+RowClue, +Index,-Retorno).
 %
+% This method finds a clue corresponding to a position
 findClue([X|_RowClue],0,X).
 findClue([_H|RowClue],Index,Retorno):-
 	Index > 0,
@@ -61,8 +68,10 @@ findClue([_H|RowClue],Index,Retorno):-
 	findClue(RowClue,IndexS,Retorno).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%documentar
 %
-% put(+Content, +Pos, +RowsClues, +ColsClues, +Grid, -NewGrid, -RowSat, -ColSat).
+% searchClue(+Clues, +Row,-Out).
 %	
+% This method searchs in a list for a painted square and checks if the painted sqquares are in order
+%
 searchClue([],[],true).
 searchClue([],[H|Row],Out):-
 	H = "X",
@@ -82,7 +91,9 @@ searchClue([H|Clues],[X|Row],Out):-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-% put(+Content, +Pos, +RowsClues, +ColsClues, +Grid, -NewGrid, -RowSat, -ColSat).
+% searchActiveClue(+Clues, +Row, -Out).
+%
+% The method searchs for continous painted squares in a list and checks for the clues
 %
 searchActiveClue([0],[],true).
 searchActiveClue([_H|_Clues],[],false).
@@ -100,6 +111,16 @@ searchActiveClue([H|Clues],[X|Row],Out):-
 	Hs is H-1,
 	searchActiveClue([Hs|Clues],Row,Out),
     !.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% checkRows(+RowN, +RowClues, +Grid, -RowSat).
+%
+% checkCols(+ColN, +ColClues, +Grid, -ColSat).
+%
+% This methods recieve a position, clues and a grid and return the satisfaction of the clues
+% Each method differs in the components given. One acts over a column and the other over a row
+%
 
 checkRows(RowN,RowClues,Grid,RowSat):-
 	rowToList(RowN,Grid,Selected),
@@ -124,6 +145,8 @@ put(Content, [RowN, ColN], RowsClues, ColsClues, Grid, NewGrid, RowSat, ColSat):
 	Cell == Content
 		;
 	replace(_Cell, ColN, Content, Row, NewRow)),
+	% Copy_term creates a copy of the new grid to operate over
+	% Then it check on the grid the clues
 	copy_term(NewGrid,Aux4Col),
 	copy_term(NewGrid,Aux4Row),
     checkRows(RowN,RowsClues,Aux4Row,RowSat),
